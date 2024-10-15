0a7e26aac97c9ceeadb90866172187121b99363d
VRCxOCULUS <88162881+VRCxOCULUS@users.noreply.github.com>
2024-10-15 08:51:57 -0400

    edit main.c

--- testing/main.c
+++ testing/main.c
@@ -1,7 +1,18 @@
 #include <nusys.h>
 
+static void vsyncCallback(int pendingTaskCount);
+
 void mainproc(void * dummy)
 {
+    nuGfxInit();
+    nuGfxFuncSet((NUGfxFunc)vsyncCallback);
+    NuGfxDisplayOn();
+
     while(1)
         ;
+}
+
+void vsyncCallback(int pendingTaskCount)
+{
+    
 }
